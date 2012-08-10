95de7ac856c340b8ec7d06c95ee3f212cd10b80e
joe miller <joeym@joeym.net>
2012-08-10 13:04:04 -0700

    added idempotency guard

--- cookbooks/osx_settings/recipes/desktop.rb
+++ cookbooks/osx_settings/recipes/desktop.rb
@@ -1,3 +1,5 @@
 # set suspend mode to RAM only and delete the sleepimage to save space on the SSD
 execute "sudo pmset -a hibernatemode 0"
-execute "sudo rm /private/var/vm/sleepimage"
+execute "sudo rm /private/var/vm/sleepimage" do
+  only_if { File.Exists? "/private/var/vm/sleepimage" }
+end
