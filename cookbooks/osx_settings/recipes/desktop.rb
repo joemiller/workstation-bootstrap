8c642d440d477caee84536dfc675d73169fb520a
joe miller <joeym@joeym.net>
2012-08-13 09:25:51 -0700

    Exists? is not a method, should be exists?

--- cookbooks/osx_settings/recipes/desktop.rb
+++ cookbooks/osx_settings/recipes/desktop.rb
@@ -1,5 +1,5 @@
 # set suspend mode to RAM only and delete the sleepimage to save space on the SSD
 execute "sudo pmset -a hibernatemode 0"
 execute "sudo rm /private/var/vm/sleepimage" do
-  only_if { File.Exists? "/private/var/vm/sleepimage" }
+  only_if { ::File.exists? "/private/var/vm/sleepimage" }
 end
