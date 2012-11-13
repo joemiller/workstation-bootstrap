926f806aa6bd56213396752019741bd4ee04c4df
joe miller <joeym@joeym.net>
2012-11-13 08:21:46 -0800

    moved opinionated sublimetext2 settings into separate recipe

--- cookbooks/apps/recipes/sublime_text.rb
+++ cookbooks/apps/recipes/sublime_text.rb
@@ -34,19 +34,3 @@ end
 link "/usr/local/bin/subl" do
   to "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
 end
-
-# setup symlinks to shared config dir on dropbox. If you don't do this
-# then you should delete it or replace with your own method.
-directory File.expand_path("~/Library/Application Support/Sublime Text 2")
-
-link File.expand_path("~/Library/Application Support/Sublime Text 2/Installed Packages") do
-  to File.expand_path("~/Dropbox/sublime-text2-settings/Installed Packages")
-end
-
-link File.expand_path("~/Library/Application Support/Sublime Text 2/Pristine Packages") do
-  to File.expand_path("~/Dropbox/sublime-text2-settings/Pristine Packages")
-end
-
-link File.expand_path("~/Library/Application Support/Sublime Text 2/Packages") do
-  to File.expand_path("~/Dropbox/sublime-text2-settings/Packages")
-end
