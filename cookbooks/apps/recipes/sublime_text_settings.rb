12a6afafebfabaf6de22cee0ebf66f90b301b61b
joe miller <joeym@joeym.net>
2013-01-13 16:30:45 -0800

    fixed up comments

--- cookbooks/apps/recipes/sublime_text_settings.rb
+++ cookbooks/apps/recipes/sublime_text_settings.rb
@@ -1,6 +1,5 @@
-
-# setup symlinks to shared config dir on dropbox. If you don't do this
-# then you should delete it or replace with your own method.
+# setup symlinks to shared config dir on dropbox. If you don't do this too
+# then you should not include this recipe in your role.
 directory File.expand_path("~/Library/Application Support/Sublime Text 2")
 
 link File.expand_path("~/Library/Application Support/Sublime Text 2/Installed Packages") do
